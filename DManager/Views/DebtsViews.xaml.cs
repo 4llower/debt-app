b92315328d9c2d7e1c0382a84a661ba6129a7026
4llower <i.4llower@gmail.com>
2020-03-31 18:58:15 +0300

    Added toolbar button.

--- DManager/Views/DebtsViews.xaml.cs
+++ DManager/Views/DebtsViews.xaml.cs
@@ -12,9 +12,24 @@ namespace DManager
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DebtsViews : TabbedPage
     {
+        [Obsolete]
         public DebtsViews()
         {
             InitializeComponent();
+
+            ToolbarItem CreateDebt = new ToolbarItem
+            {
+                Text = "help",
+                Order = ToolbarItemOrder.Primary,
+                Priority = 0,
+                Icon = new FileImageSource
+                {
+                    File = "iconAdd.png"
+                }
+            };
+
+            ToolbarItems.Add(CreateDebt);
+
         }
     }
 }
