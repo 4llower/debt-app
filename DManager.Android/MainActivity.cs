955115987ce286c65261a50c68950fd9c5f2ed26
4llower <i.4llower@gmail.com>
2020-04-10 22:53:26 +0300

    Fix bug with Icon, 1.0 version of application.

--- DManager.Android/MainActivity.cs
+++ DManager.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DManager.Droid
 {
-    [Activity(Label = "DManager", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DManager", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
